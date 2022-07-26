a294cd8098bc9ad75ec069211111baaa3a2d62a4
Ken Yamada <yamakenth@gmail.com>
2022-07-26 17:40:36 +0700

    Add more indexed access types for reusability

--- server/src/schema/category/resolver.ts
+++ server/src/schema/category/resolver.ts
@@ -1,7 +1,7 @@
 import { categories } from "../../sampleData";
 import { Category } from "./type";
 
-export function getCategory(id: string): Category | undefined {
+export function getCategory(id: Category["id"]): Category | undefined {
   return categories.find((category) => category.id === id);
 }
 
