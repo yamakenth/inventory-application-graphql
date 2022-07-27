95eb4df4c2fa76fe384b3984cde774e0a25e3bb1
Ken Yamada <yamakenth@gmail.com>
2022-07-27 15:38:12 +0700

    Add cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,15 @@
 import "dotenv/config";
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
+import cors from "cors";
 import connectDB from "./config/db";
 import { schema } from "./schema";
 
 const app = express();
 
 connectDB();
+
+app.use(cors());
 
 app.use(
   "/graphql",
